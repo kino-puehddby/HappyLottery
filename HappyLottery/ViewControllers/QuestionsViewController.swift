cee6e88c608e69cc5e2ffd166488413bc796ed7f
sugitahisaya <sugitahisaya@team-lab.com>
2019-02-15 18:50:39 +0900

    計算処理WIP

--- HappyLottery/ViewControllers/QuestionsViewController.swift
+++ HappyLottery/ViewControllers/QuestionsViewController.swift
@@ -36,9 +36,9 @@ final class QuestionsViewController: UIViewController {
         
         var rate: Double {
             switch self {
-            case .good: return 1.2
+            case .good: return 1.5
             case .fair: return 1
-            case .poor: return 0.8
+            case .poor: return 0.75
             }
         }
     }
@@ -50,9 +50,9 @@ final class QuestionsViewController: UIViewController {
         
         var rate: Double {
             switch self {
-            case .good: return 1.2
+            case .good: return 2
             case .fair: return 1
-            case .poor: return 0.8
+            case .poor: return 0.5
             }
         }
     }
@@ -64,15 +64,33 @@ final class QuestionsViewController: UIViewController {
         case highSchool
         case university
         
-        var rate: Double {
+        var min: Double {
             switch self {
-            case .preschool: return 500
-            case .elementarySchool: return 2000
-            case .juniorHighSchool: return 5000
-            case .highSchool: return 7000
-            case .university: return 10000
+            case .preschool: return 100
+            case .elementarySchool: return 500
+            case .juniorHighSchool: return 2000
+            case .highSchool: return 3000
+            case .university: return 5000
             }
         }
+        
+        var max: Double {
+            switch self {
+            case .preschool: return 1000
+            case .elementarySchool: return 3000
+            case .juniorHighSchool: return 8000
+            case .highSchool: return 12000
+            case .university: return 15000
+            }
+        }
+    }
+    
+    enum Probability {
+        case great
+        case good
+        case usually
+        case bad
+        case shit
     }
     
     override func viewDidLoad() {
@@ -109,21 +127,69 @@ final class QuestionsViewController: UIViewController {
                 (
                     economyRate: self.economyGrade.value.rate,
                     effortRate: self.effortGrade.value.rate,
-                    schoolRate: self.schoolStage.value.rate
+                    min: self.schoolStage.value.min,
+                    max: self.schoolStage.value.max
                 )
             }
             .subscribe(onNext: { [unowned self] arg in
                 self.calculate(
                     economyRate: arg.economyRate,
                     effortRate: arg.effortRate,
-                    schoolRate: arg.schoolRate
+                    min: arg.min,
+                    max: arg.max
                 )
             })
             .disposed(by: disposeBag)
     }
     
-    func calculate(economyRate: Double, effortRate: Double, schoolRate: Double) {
-        // TODO: 計算処理
+    func calculate(economyRate: Double, effortRate: Double, min: Double, max: Double) {
+        // probability = 確率が良かったかどうか（努力して景気がいいほど当たりやすくなる）
+        let probability: Probability = {
+            let count = Double.random(in: 1...100)
+            if count <= 5 * economyRate * effortRate {
+                return .great
+            } else if count <= 25 * economyRate * effortRate {
+                return .good
+            } else if count <= 75 {
+                return .usually
+            } else if count <= 95 * (1 / economyRate) * (1 / effortRate)  {
+                return .bad
+            } else {
+                return .shit
+            }
+        }()
+        
+        // 運が良かったらmaxが増える
+        let fixMax: Double = {
+            switch probability {
+            case .great: return max * 1.5
+            case .good: return max * 1.2
+            case .usually: return max
+            case .bad: return max * 0.8
+            case .shit: return max * 0.5
+            }
+        }()
+        let fixMin: Double = {
+            switch probability {
+            case .great: return min * 1.5
+            case .good: return min * 1.2
+            case .usually: return min
+            case .bad: return min * 0.8
+            case .shit: return min * 0.5
+            }
+        }()
+        
+        // random = 学校段階によってランダムなお年玉がランダムに決まる
+        var random = Double.random(in: fixMin ... fixMax)
+        switch schoolStage.value {
+        case .preschool:
+            random = round(random / 100) * 100
+        case .elementarySchool:
+            random = round(random / 500) * 500
+        default:
+            random = round(random / 1000) * 1000
+        }
+        
         perform(segue: StoryboardSegue.Questions.showResult)
     }
 }
